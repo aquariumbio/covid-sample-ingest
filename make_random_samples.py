a2e8540d33273a22b2660b9ce4b72f3071fe28bf
Devin Strickland <strcklnd@uw.edu>
2020-11-18 20:51:27 -0500

    can make any number of random samples

--- make_random_samples.py
+++ make_random_samples.py
@@ -1,8 +1,18 @@
 import csv
+import argparse
+
 from util.random_samples import create_random_sample_csv
 
 def main():
-    create_random_sample_csv()
+    args = get_args()
+    create_random_sample_csv(n_samples=int(args.n_samples))
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--n_samples",
+                        help="the number of samples to make",
+                        default=96)
+    return parser.parse_args()
 
 if __name__ == "__main__":
     main()
