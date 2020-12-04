75915a7219f28bd9399b2ab4d91313dc1142a065
Devin Strickland <strcklnd@uw.edu>
2020-12-04 14:01:38 -0500

    change number of samples

--- test_load.sh
+++ test_load.sh
@@ -1,4 +1,4 @@
+python make_random_samples.py -n 16
 python make_random_samples.py -n 5
-python make_random_samples.py -n 3
-python make_random_samples.py -n 7
+python make_random_samples.py -n 9
 python load_samples.py -s laptop
