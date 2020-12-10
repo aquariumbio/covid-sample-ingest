0a97556f51083928d9d078a04bd9f94aa50f0a0c
Devin Strickland <strcklnd@uw.edu>
2020-12-10 14:52:12 -0500

    updated sample loader to deal with new format

--- util/sample_loader.py
+++ util/sample_loader.py
@@ -1,20 +1,42 @@
 import os
-import csv
+import re
 import glob
 
 def load_all_in_path(session, path, archive_path):
     new_samples_by_filename = {}
-    for file_path in glob.glob(os.path.join(path, "*.csv")):
-        new_samples = load_from_csv(file_path, session, "Specimen", "Nasopharyngeal Swab", "Ingest")
+    for file_path in glob.glob(os.path.join(path, "*.txt")):
+        new_samples = load_from_file(file_path, session, "Specimen", "Nasopharyngeal Swab", "Ingest")
         filename = os.path.basename(file_path)
         new_samples_by_filename[filename] = new_samples
         os.rename(file_path, os.path.join(archive_path, filename))
     return new_samples_by_filename
 
-def load_from_csv(file_path, session, st_name, ot_name, location):
-    with open(file_path) as csvfile:
-        sample_data = csv.DictReader(csvfile)
-        return load_samples_and_items(sample_data, session, st_name, ot_name, location)
+def load_from_file(file_path, session, st_name, ot_name, location):
+    sample_data = format_data(file_path)
+    return load_samples_and_items(sample_data, session, st_name, ot_name, location)
+
+def format_data(file_path):
+    with open(file_path) as f:
+        sample_data = []
+        lines = f.readlines()
+        filename = os.path.basename(file_path)
+
+        for line in lines:
+            mtch = re.search(r"(\d{2})\s+([ABCDEFGH])\s+(.+)", line)
+            if mtch:
+                rack_location = mtch.group(2) + mtch.group(1)
+                rack_barcode = re.search(r"\d+-(\d+).txt", filename).group(1)
+                data = {
+                    "name": "{}_{}".format(rack_barcode, rack_location),
+                    "description": "Specimen imported from file {}".format(filename),
+                    "project": "Test",
+                    "Specimen Barcode": mtch.group(3),
+                    "Ingest Rack Barcode": rack_barcode,
+                    "Ingest Rack Location": rack_location
+                }
+                sample_data.append(data)
+    return sample_data
+
 
 def load_samples_and_items(sample_data, session, st_name, ot_name, location):
     sample_type = session.SampleType.find_by_name(st_name)
